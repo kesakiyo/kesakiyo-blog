af8aa642591f3eb1c611d0257dbadd603f59105f
kesakiyo <kesakiyo@naver.com>
2019-08-17 01:58:31 +0900

    Add skeleton of global-nav component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,60 +1,13 @@
 import React from "react"
-import { Link } from "gatsby"
+
+import GlobalNav from "./global-nav"
 
 class Layout extends React.Component {
   render() {
-    const { location, title, children } = this.props
-    const rootPath = `${__PATH_PREFIX__}/`
-    let header
-
-    if (location.pathname === rootPath) {
-      header = (
-        <h1
-          style={{
-            marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
-        </h1>
-      )
-    } else {
-      header = (
-        <h3
-          style={{
-            fontFamily: `Montserrat, sans-serif`,
-            marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
-        </h3>
-      )
-    }
+    const { children } = this.props
     return (
-      <div
-        style={{
-          marginLeft: `auto`,
-          marginRight: `auto`,
-        }}
-      >
-        <header>{header}</header>
+      <div>
+        <GlobalNav />
         <main>{children}</main>
         <footer>
           © {new Date().getFullYear()}, Built with
